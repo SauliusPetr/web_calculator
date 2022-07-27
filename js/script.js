0cf9de74e6f70af6aea0696c54a7f23a48a02a1b
Saulius <petruseviciuss@gmail.com>
2022-07-27 22:00:45 +0300

    Add math functions

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//add
+function add(var_one,var_two){
+    return var_one+var_two;
+}
+
+//subtract
+function subtract(var_one,var_two){
+    return var_one-var_two;
+}
+
+//multiply
+function multiply(var_one,var_two){
+    return var_one*var_two;
+}
+
+//divide
+function divide(var_one,var_two){
+    return var_one/var_two;
+}
