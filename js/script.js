50784554aeefecfec9e1192287f5ee960fd35567
Saulius <petruseviciuss@gmail.com>
2022-07-27 22:08:00 +0300

    Add operate function calling math functions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 //add
-function add(var_one,var_two){
-    return var_one+var_two;
+function add(var_one, var_two) {
+    return var_one + var_two;
 }
 
 //subtract
-function subtract(var_one,var_two){
-    return var_one-var_two;
+function subtract(var_one, var_two) {
+    return var_one - var_two;
 }
 
 //multiply
-function multiply(var_one,var_two){
-    return var_one*var_two;
+function multiply(var_one, var_two) {
+    return var_one * var_two;
 }
 
 //divide
-function divide(var_one,var_two){
-    return var_one/var_two;
+function divide(var_one, var_two) {
+    return var_one / var_two;
+}
+
+function operate(operator, var_one, var_two) {
+    switch (operator) {
+        case '+':
+            return add(var_one, var_two);
+        case '-':
+            return subtract(var_one, var_two);
+        case '*':
+            return multiply(var_one, var_two);
+        case '/':
+            return divide(var_one, var_two);
+        default:
+            return "OOPS, something went wrong!";
+            break;
+    }
 }
