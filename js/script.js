738aa1709e7f03741952e179599012fc25a4210c
Saulius <petruseviciuss@gmail.com>
2022-07-31 16:15:24 +0300

    Add erase with backspace func and keysupport

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const expressionArray = ['-', '+', '*', '/'];
 const isNum = /[0-9]/g;
+
 let allValues = {
     numberOne: '',
     operator: '',
@@ -48,70 +49,97 @@ function updateDisplay(elem) {
     }
 }
 
-function selectNumber() {
-    let numberButtons = document.querySelectorAll('.number');
+function updateValues(numBtn) {
+    if (numBtn.textContent.match(isNum)) {
+        if (allValues.operator == '') {
+            if (allValues['numberOne'] == '' || allValues['numberOne'] == '0') {
+                updateDisplay('Clear');
+                clearFormula();
+            }
+            allValues['numberOne'] += numBtn.textContent;
+        } else {
+            allValues['numberTwo'] += numBtn.textContent;
+        }
+        updateDisplay();
+    }
+}
+
+function clickNumber() {
+    let numberButtons = document.querySelectorAll('.numbers > button');
     numberButtons.forEach(numBtn => {
         numBtn.addEventListener('click', () => {
-            if (numBtn.textContent.match(isNum)) {
-                if (allValues.operator == '') {
-                    if (allValues['numberOne'] == '') {
-                        updateDisplay('Clear');
-                    }
-                    allValues['numberOne'] += numBtn.textContent;
-                } else {
-                    allValues['numberTwo'] += numBtn.textContent;
-                }
-                updateDisplay();
+            updateValues(numBtn);
+        });
+    });    
+}
+
+function pressNumber(){
+    let numberButtons = document.querySelectorAll('.numbers > button');
+    document.addEventListener('keydown',(pressedKey)=>{
+        numberButtons.forEach((numBtn)=>{
+            if(numBtn.dataset.key == pressedKey.keyCode){
+                updateValues(numBtn);
             }
-        })
+        });
+    });
+}
+
+function undoNumber(){
+    document.addEventListener('keydown',(pressedKey)=>{
+        if(pressedKey.keyCode == 8 ){
+            allValues['numberOne'] = allValues['numberOne'].slice(0,allValues['numberOne'].length-1);
+            updateDisplay();
+        }
     });
 }
 
 function selectOperator() {
-    let operatorButtons = document.querySelectorAll('.operator');
-    operatorButtons.forEach(opBtn => {
-        opBtn.addEventListener('click', () => {
-            if (expressionArray.includes(opBtn.textContent) && allValues['numberTwo'] == '' && allValues['numberOne'] != '') {
-                allValues['operator'] = opBtn.textContent;
-                updateDisplay();
-            }
-            else if (expressionArray.includes(opBtn.textContent) && allValues['numberTwo'] != '' && allValues['numberOne'] != '') {
-                calculate();
-                allValues['operator'] = opBtn.textContent;
-                updateDisplay();
-            }
-            else if (opBtn.textContent == 'Clear') {
-                clearFormula();
-            } else if (opBtn.textContent == '=') {
-                calculate();
-            }
-        })
-    });
-}
+            let operatorButtons = document.querySelectorAll('.operator');
+            operatorButtons.forEach(opBtn => {
+                opBtn.addEventListener('click', () => {
+                    if (expressionArray.includes(opBtn.textContent) && allValues['numberTwo'] == '' && allValues['numberOne'] != '') {
+                        allValues['operator'] = opBtn.textContent;
+                        updateDisplay();
+                    }
+                    else if (expressionArray.includes(opBtn.textContent) && allValues['numberTwo'] != '' && allValues['numberOne'] != '') {
+                        calculate();
+                        allValues['operator'] = opBtn.textContent;
+                        updateDisplay();
+                    }
+                    else if (opBtn.textContent == 'Clear') {
+                        clearFormula();
+                    } else if (opBtn.textContent == '=') {
+                        calculate();
+                    }
+                })
+            });
+        }
 function calculate() {
-    let answer = '0';
-    if (allValues['numberTwo'] == '' && allValues['numberOne'] != '') {
-        answer = allValues['numberOne'];
-    } else if (allValues['numberTwo'] != '') {
-        if (allValues['operator'] == '/' && allValues['numberOne'] == '0') {
-            answer = 0;
+            let answer = '0';
+            if (allValues['numberTwo'] == '' && allValues['numberOne'] != '') {
+                answer = allValues['numberOne'];
+            } else if (allValues['numberTwo'] != '') {
+                if (allValues['operator'] == '/' && allValues['numberOne'] == '0') {
+                    answer = 0;
+                }
+                else {
+                    answer = operate(allValues.operator, allValues.numberOne, allValues.numberTwo).toFixed(3);
+                }
+            }
+            clearFormula();
+            allValues['numberOne'] += answer;
+            updateDisplay();
         }
-        else {
-            answer = operate(allValues.operator, allValues.numberOne, allValues.numberTwo).toFixed(3);
-        }
-    }
-    clearFormula();
-    allValues['numberOne'] += answer;
-    updateDisplay();
-}
 
 function clearFormula() {
-    updateDisplay('Clear');
-    allValues = {
-        numberOne: '',
-        operator: '',
-        numberTwo: ''
-    };
-}
-selectNumber();
+            updateDisplay('Clear');
+            allValues = {
+                numberOne: '',
+                operator: '',
+                numberTwo: ''
+            };
+        }
+clickNumber();
 selectOperator();
+pressNumber();
+undoNumber();
